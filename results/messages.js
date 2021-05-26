682f48add76ae60defad41dde0fe372ab211f327
Elise Muellerleile <eliamue@live.com>
2021-05-26 16:03:05 -0500

    no template usey uwu

--- results/messages.js
+++ results/messages.js
@@ -1,59 +1,41 @@
-export const hpMessage = {
-    poor: 'You leave nothing' ,
-    modest: 'Your family is... fine',
-    rich: 'Your family is in the 1% yo',
-};
+// export const hpMessage = {
+//     poor: 'You leave nothing' ,
+//     modest: 'Your family is... fine',
+//     rich: 'Your family is in the 1% yo',
+// };
 
-export const hpMessage = {
-    poor: 'You leave nothing' ,
-    modest: 'Your family is... fine',
-    rich: 'Your family is in the 1% yo',
-};
+// // these are pure functions, as they take in a user and returns a string, bing bang boom
+// function evaluateHealth(user) {
+//     if (user.hp > 35) {
+//         return 'healthy';
+//     }
+//     if (user.hp > 0) {
+//         return 'frail';
+//     }
+//     return 'you dead';
+// }
 
-export const hpMessage = {
-    poor: 'You leave nothing' ,
-    modest: 'Your family is... fine',
-    rich: 'Your family is in the 1% yo',
-};
+// function evaluateWealth(user) {
+//     if (user.gold > 40) {
+//         return 'rich';
+//     }
+//     if (user.gold > 20) {
+//         return 'modest';
+//     }
+//     return 'you poor as hecc';
+// }
 
-export const hpMessage = {
-    poor: 'You leave nothing' ,
-    modest: 'Your family is... fine',
-    rich: 'Your family is in the 1% yo',
-};
+// export function getEndingMessage(user) {
+//     const health = evaluateHealth(user) {};
+//     const wealth = evaluateWealth(user) {};
 
-// these are pure functions, as they take in a user and returns a string, bing bang boom
-function evaluateHealth(user) {
-    if (user.hp > 35) {
-        return 'healthy';
-    }
-    if (user.hp > 0) {
-        return 'frail';
-    }
-    return 'you dead';
-}
+//     let wealthMessage;
 
-function evaluateWealth(user) {
-    if (user.gold > 40) {
-        return 'rich';
-    }
-    if (user.gold > 20) {
-        return 'modest';
-    }
-    return 'you poor as hecc';
-}
+//     if(user.hp <= 0) {
+//         wealthMessage = deadGoldMessages[wealth];
+//     } else {
+//         wealthMessage = aliveGoldMessages[wealth];
+//     }
 
-export function getEndingMessage(user) {
-    const health = evaluateHealth(user) {};
-    const wealth = evaluateWealth(user) {};
-
-    let wealthMessage;
-
-    if(user.hp <= 0) {
-        wealthMessage = deadGoldMessages[wealth];
-    } else {
-        wealthMessage = aliveGoldMessages[wealth];
-    }
-
-    return `Congrats, ${user.name} the ${user.class}! ${hpMessage[health]} and ${wealthMessage}`;
-}
+//     return `Congrats, ${user.name} the ${user.class}! ${hpMessage[health]} and ${wealthMessage}`;
+// }
